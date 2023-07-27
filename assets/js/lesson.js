5b006b62b31dd3705348f96bd5a663bb58b4a71f
Edward J. Parkinson <e.parkinson@soton.ac.uk>
2023-07-27 14:15:31 +0100

    foldable js for hidden

--- assets/js/lesson.js
+++ assets/js/lesson.js
@@ -18,6 +18,21 @@ $(".solution").each(function() {
     h2.append("<span class='fold-unfold glyphicon glyphicon-collapse-down'></span>");
 });
 
+// Handle foldable callout boxes (on click and at start)
+$(".hidden").click(function(event) {
+    var trigger = $(event.target).has(".fold-unfold").length > 0
+               || $(event.target).filter(".fold-unfold").length > 0;
+    if (trigger) {
+        $(">*:not(h2)", this).toggle(400);
+        $(">h2>span.fold-unfold", this).toggleClass("glyphicon-collapse-down glyphicon-collapse-up");
+        event.stopPropagation();
+    }
+});
+$(".hidden").each(function() {
+    $(">*:not(h2)", this).toggle();
+    var h2 = $("h2:first", this);
+    h2.append("<span class='fold-unfold glyphicon glyphicon-collapse-down'></span>");
+});
 
 // Handle searches.
 // Relies on document having 'meta' element with name 'search-domain'.
